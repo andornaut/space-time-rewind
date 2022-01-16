d0e7db2f1efc496b013d78c6509dcf3865be3385
andornaut <andornaut@users.noreply.github.com>
2022-01-16 10:27:35 -0500

    Split main/lib

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+// Requires Rust from the nightly channel as of rustc 1.59.0-nightly (e012a191d 2022-01-06)
+#![feature(mixed_integer_ops)]
+
+use anyhow::Result;
+use app::App;
+use session::Session;
+
+mod app;
+mod clock;
+mod color;
+mod command;
+mod game;
+mod session;
+mod view;
+
+pub fn run() -> Result<()> {
+    let mut session = Session::begin()?;
+
+    App::default().run(&mut session)?;
+
+    Ok(session.end()?)
+}
