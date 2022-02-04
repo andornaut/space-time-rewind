fd50aa81a8483e94583e95a4e43b5ab96842cfc7
andornaut <andornaut@users.noreply.github.com>
2022-02-04 17:40:09 -0500

    Use blocking read() instead of poll() to get input

--- src/app/input.rs
+++ src/app/input.rs
@@ -1,12 +1,9 @@
 use super::command::Command;
-use crossterm::event::{poll, read, Event};
+use crossterm::event::{read, Event};
 use std::{
     sync::mpsc::{Receiver, Sender},
     thread,
-    time::Duration,
 };
-
-const INPUT_POLL_TIMEOUT_MS: u64 = 50;
 
 pub fn receive_input_commands(rx: &Receiver<Command>) -> Vec<Command> {
     let mut commands = Vec::new();
@@ -22,14 +19,14 @@ pub fn receive_input_commands(rx: &Receiver<Command>) -> Vec<Command> {
 }
 
 pub fn send_input_commands(tx: Sender<Command>) {
-    let timeout = Duration::from_millis(INPUT_POLL_TIMEOUT_MS);
     thread::spawn(move || loop {
-        if poll(timeout).expect("Can poll for input") {
-            // `poll()` returned true, so an event is available,
-            // so the following call to `read()` will not block.
-            if let Event::Key(key) = read().expect("Can read key events") {
-                tx.send(Command::from(key)).unwrap();
+        // Blocking read
+        if let Event::Key(key) = read().expect("Can read key events") {
+            let command = Command::from(key);
+            if let Command::Continue = command {
+                continue;
             }
+            tx.send(command).unwrap();
         }
     });
 }
