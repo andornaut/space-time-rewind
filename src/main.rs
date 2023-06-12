57846a61b3c39c78183503ed7bc8762a2432f862
andornaut <andornaut@users.noreply.github.com>
2023-06-12 10:17:03 -0400

    Update deps and do not use mod files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Requires Rust from the nightly channel as of rustc 1.59.0-nightly (e012a191d 2022-01-06)
-#![feature(mixed_integer_ops)]
+//#![feature(mixed_integer_ops)]
 // Allow modules to have the same name as their parent module
-#![allow(clippy::module_inception)]
+//#![allow(clippy::module_inception)]
 // Show more lint warnings
 #![warn(clippy::all, clippy::pedantic)]
 use anyhow::Result;
